93e17647fe63a69b0d9aa5a1b5e0bb1708254e5b
NandiniSannareddy <150068076+NandiniSannareddy@users.noreply.github.com>
2024-07-09 20:32:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+            const menu = document.getElementById('menu-icon');
+            const cls = document.getElementsByClassName('navbar');
+
+            menu.onclick = function() {
+                for (let i = 0; i < cls.length; i++) {
+                    cls[i].style.display = cls[i].style.display === 'block' ? 'none' : 'block';
+                }
+               
+            };
+        });
